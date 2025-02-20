1a1579feae831949133b9afd4355f7666f14061a
estraaron <aestrada@apexsystems.com>
2025-02-20 11:13:25 -0700

    clean code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { select, Store } from '@ngrx/store';
-import { createCustomer, retrieveCustomer } from './state/donor/donor.actions';
-import { selectDonorDetails } from './state/donor/donor.selectors';
+import { Component, } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CreateDonorComponent } from "./components/create-donor/create-donor.component";
-import { DonationDetailsComponent } from "./components/donation-details/donation-details.component";
-import { ConfirmationComponent } from "./components/confirmation/confirmation.component";
-import { Observable, Subscription } from 'rxjs';
-import { selectDonationState, selectPrice, selectPriceDetails } from './state/donation/donation.selectors';
-import { getPrice } from './state/donation/donation.actions';
-import { PaymentMethodComponent } from "./components/app-payment-method/app-payment-method.component";
-import { AppThankYouComponent } from "./components/app-thank-you/app-thank-you.component";
-import { PaymentEventService } from './services/payment-event.service';
-
 
 import { CrezcoDonationAppComponent } from 'crezco-donation-app'; // 📌 Importar el componente de la librería
 
